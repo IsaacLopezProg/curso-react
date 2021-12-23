3f23ccfcd72ad8e6140c9df35a8c85a2f7ad1a17
Isaac Lopez PC <isaaclopezprog@gmail.com>
2021-12-23 15:52:08 -0400

    empenzando con redux

--- crudredux/src/App.js
+++ crudredux/src/App.js
@@ -1,12 +1,37 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 
 import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProductos from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+
+// ROUTING
+import {
+  BrowserRouter,
+  Routes, // Just Use Routes instead of "Switch"
+  Route,
+} from "react-router-dom";
+
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 function App() {
   return (
-      <Fragment>
-      <Header />
-      </Fragment>
+      <BrowserRouter>
+        <Provider store={store}>
+              <Header />
+                <div className="container mt-5">
+                <Routes>
+                    <Route exact path="/" element={<Productos />} ></Route>
+                    <Route exact path="/productos/nuevo" element={<NuevoProductos />}></Route>
+                    <Route exact path="/productos/editar/:id" element={<EditarProducto />} ></Route>
+                </Routes>
+            </div>
+            </Provider>
+        </BrowserRouter>
   );
 }
 
